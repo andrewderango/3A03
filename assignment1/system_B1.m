bf5c8b1e6ebd91c035ef6925a6bed8c63c7df0ef
Andrew De Rango <drewderango@gmail.com>
2024-09-21 14:02:30 -0400

    fix incorrect commenting for y(1)=0

--- assignment1/system_B1.m
+++ assignment1/system_B1.m
@@ -14,7 +14,7 @@ function y = system_B1(n,x)
     % with zero initial conditions, i.e., the input signal x[n] = 0 before the start of the given input array x
     
     y = zeros(size(x)); % initialize array for output signal y[n]
-    y(1) = sin(0); % IC
+    y(1) = 0; % unknown x[n-1] but must be 0 because n=0 and sin(0)=0
     
     for i = 2:length(n) % start at 2 to skip IC (always first index)
         y(i) = sin(n(i) * x(i-1)); % solve difference equation
