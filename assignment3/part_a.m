821a960c1ee674f6e21db9327c581d9f4a8aa4ad
Andrew De Rango <drewderango@gmail.com>
2024-11-10 00:48:32 -0500

    complete part a2

--- assignment3/part_a.m
+++ assignment3/part_a.m
@@ -2,26 +2,40 @@ clc;
 clear;
 close all;
 
-% load data
+% load data and get t vector
 load('BFVdata_assignment3.mat');
-
-% get time vector for plotting
 N = length(BFVdu);
 t = (0:N-1)/BFV_Fs;
 
-% plot the signal
+% PART A1
+% plot raw signal with its magnitude spectrum
+[Mx_BFVdu, phx_BFVdu, f_BFVdu] = fourier_dt(BFVdu, BFV_Fs, 'half');
 figure;
 subplot(2,1,1);
 plot(t, BFVdu, 'LineWidth', 1.25);
-title('Entire Signal: BFVdu');
+title('Time-Domain Signal: Full BFVdu');
 xlabel('Time (seconds)');
 ylabel('Blood Flow Velocity (m/s)');
-
-% compute and plot magnitude spectrum
-[Mx_BFVdu, phx_BFVdu, f_BFVdu] = fourier_dt(BFVdu, BFV_Fs, 'half');
 subplot(2,1,2);
 plot(f_BFVdu, Mx_BFVdu, 'LineWidth', 1.5);
-title('Magnitude Spectrum: BFVdu');
+title('Magnitude Spectrum: Full BFVdu');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+xlim([0 10]);
+
+% PART A2
+% plot first half of signal with its magnitude spectrum
+half_BFVdu = BFVdu(1:N/2);
+[Mx_half_BFVdu, phx_half_BFVdu, f_half_BFVdu] = fourier_dt(half_BFVdu, BFV_Fs, 'half');
+figure;
+subplot(2,1,1);
+plot(t(1:N/2), half_BFVdu, 'LineWidth', 1.25);
+title('Time-Domain Signal: Half 1 of BFVdu');
+xlabel('Time (seconds)');
+ylabel('Blood Flow Velocity (m/s)');
+subplot(2,1,2);
+plot(f_half_BFVdu, Mx_half_BFVdu, 'LineWidth', 1.5);
+title('Magnitude Spectrum: Half 1 of BFVdu');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
 xlim([0 10]);
