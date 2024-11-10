9b5c5ebbc5c51ff71e32435d21731a56df552de5
Andrew De Rango <drewderango@gmail.com>
2024-11-10 00:39:22 -0500

    minor plot adj

--- assignment3/part_a.m
+++ assignment3/part_a.m
@@ -12,15 +12,16 @@ t = (0:N-1)/BFV_Fs;
 % plot the signal
 figure;
 subplot(2,1,1);
-plot(t, BFVdu);
+plot(t, BFVdu, 'LineWidth', 1.25);
 title('Entire Signal: BFVdu');
 xlabel('Time (seconds)');
 ylabel('Blood Flow Velocity (m/s)');
 
 % compute and plot magnitude spectrum
-[Mx_BFVdu, phx_BFVdu, f_BFVdu] = fourier_dt(BFVdu, BFV_Fs, 'full');
+[Mx_BFVdu, phx_BFVdu, f_BFVdu] = fourier_dt(BFVdu, BFV_Fs, 'half');
 subplot(2,1,2);
-plot(f_BFVdu, Mx_BFVdu);
+plot(f_BFVdu, Mx_BFVdu, 'LineWidth', 1.5);
 title('Magnitude Spectrum: BFVdu');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
+xlim([0 10]);
