75a47bdf26107a998ddbb91a4f0230a3368eeda4
Garret Langlois <gaslanglois@gmail.com>
2024-11-14 10:56:48 -0500

    added some changes

--- assignment3/part_b.m
+++ assignment3/part_b.m
@@ -24,7 +24,7 @@ function part_b
     disp("EEG 1 - Band Power");
 
     %Delta band
-    band_power_delta = sum(Mx1(f1 >= 0 & f1 < 3));
+    band_power_delta = sum(Mx1(f1 >= 0 & f1 < 3).^2);
 
     band_power_delta_normalized = band_power_delta / (3 - 0);
 
@@ -33,7 +33,7 @@ function part_b
     disp(['Delta Normalized: ', num2str(band_power_delta_normalized)]);
 
     %Theta band
-    band_power_theta = sum(Mx1(f1 >= 3 & f1 < 8));
+    band_power_theta = sum(Mx1(f1 >= 3 & f1 < 8).^2);
 
     band_power_theta_normalized = band_power_theta / (5 - 3);
 
@@ -42,7 +42,7 @@ function part_b
     disp(['Theta Normalized: ', num2str(band_power_theta_normalized)]);
 
     %Alpha band
-    band_power_alpha = sum(Mx1(f1 >= 8 & f1 < 13));
+    band_power_alpha = sum(Mx1(f1 >= 8 & f1 < 13).^2);
 
     band_power_alpha_normalized = band_power_alpha / (13 - 8);
 
@@ -51,7 +51,7 @@ function part_b
     disp(['Alpha Normalized: ', num2str(band_power_alpha_normalized)]);
 
     %Beta band
-    band_power_beta = sum(Mx1(f1 >= 13 & f1 < 25));
+    band_power_beta = sum(Mx1(f1 >= 13 & f1 < 25).^2);
 
     band_power_beta_normalized = band_power_beta / (25 - 13);
 
@@ -60,7 +60,7 @@ function part_b
     disp(['Beta Normalized: ', num2str(band_power_beta_normalized)]);
 
     %Gamma band
-    band_power_gamma = sum(Mx1(f1 >= 25 & f1 <= 100));
+    band_power_gamma = sum(Mx1(f1 >= 25 & f1 <= 100).^2);
 
     band_power_gamma_normalized = band_power_gamma / (100 - 25);
 
@@ -86,7 +86,7 @@ function part_b
     disp("EEG 2 - Band Power");
 
     %Delta band
-    band_power_delta = sum(Mx2(f2 >= 0 & f2 < 3));
+    band_power_delta = sum(Mx2(f2 >= 0 & f2 < 3).^2);
 
     band_power_delta_normalized = band_power_delta / (3 - 0);
 
@@ -95,7 +95,7 @@ function part_b
     disp(['Delta Normalized: ', num2str(band_power_delta_normalized)]);
 
     %Theta band
-    band_power_theta = sum(Mx2(f2 >= 3 & f2 < 8));
+    band_power_theta = sum(Mx2(f2 >= 3 & f2 < 8).^2);
 
     band_power_theta_normalized = band_power_theta / (5 - 3);
 
@@ -104,7 +104,7 @@ function part_b
     disp(['Theta Normalized: ', num2str(band_power_theta_normalized)]);
 
     %Alpha band
-    band_power_alpha = sum(Mx2(f2 >= 8 & f2 < 13));
+    band_power_alpha = sum(Mx2(f2 >= 8 & f2 < 13).^2);
 
     band_power_alpha_normalized = band_power_alpha / (13 - 8);
 
@@ -113,7 +113,7 @@ function part_b
     disp(['Alpha Normalized: ', num2str(band_power_alpha_normalized)]);
 
     %Beta band
-    band_power_beta = sum(Mx2(f2 >= 13 & f2 < 25));
+    band_power_beta = sum(Mx2(f2 >= 13 & f2 < 25).^2);
 
     band_power_beta_normalized = band_power_beta / (25 - 13);
 
@@ -122,7 +122,7 @@ function part_b
     disp(['Beta Normalized: ', num2str(band_power_beta_normalized)]);
 
     %Gamma band
-    band_power_gamma = sum(Mx2(f2 >= 25 & f2 <= 100));
+    band_power_gamma = sum(Mx2(f2 >= 25 & f2 <= 100).^2);
 
     band_power_gamma_normalized = band_power_gamma / (100 - 25);
 
