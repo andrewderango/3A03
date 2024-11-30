201a3e50f2853827e0a2928289ae16d8c93ff51f
Ethan Otteson <eotteson04@gmail.com>
2024-11-30 16:31:26 -0500

    added EEG filters and updated plotting function

--- assignment4/main.m
+++ assignment4/main.m
@@ -6,11 +6,12 @@ close all;
 % EEG Signal
 
 EEG_file = load('EEGdata_assignment4.mat');
-EEG_filter_FIR = load('testFilter.mat');
-EEG_filter_IIR = load('testFilter.mat');
+EEG_filter_FIR = load('EGG_FIR.mat');
+EEG_filter_IIR = load('EGG_IIR.mat');
 
-plot_all(EEG_file.EEGa4, EEG_file.EEG_Fs,'EEG', 'Voltage (μV)', EEG_filter_FIR.testFilter, 'FIR Bandstop Filter');
-plot_all(EEG_file.EEGa4, EEG_file.EEG_Fs,'EEG', 'Voltage (μV)', EEG_filter_IIR.testFilter, 'IIR Bandstop Filter');
+plot_all(EEG_file.EEGa4, EEG_file.EEG_Fs,'EEG', 'Voltage (μV)', ...
+EEG_filter_IIR.EGG_IIR, 'IIR Bandstop Filter', ...
+EEG_filter_FIR.EGG_FIR, 'FIR Bandstop Filter', 1);
 
 % VGRF Signal
 
@@ -18,15 +19,9 @@ VGRF_file = load('VGRFdata_assignment4.mat');
 VGRF_filter_FIR = load('testFilter.mat');
 VGRF_filter_IIR = load('IIR_bandpass.mat');
 
-
-
-%FIR Bandpass
-plot_all(VGRF_file.VGRF, VGRF_file.VGRF_Fs,'VGRF', 'Force (N)', VGRF_filter_FIR.testFilter, 'FIR Bandpass Filter');
-
-%IIR Bandpass
-plot_all(VGRF_file.VGRF, VGRF_file.VGRF_Fs,'VGRF', 'Force (N)', VGRF_filter_IIR.F, 'IIR Bandpass Filter');
-
-
+plot_all(VGRF_file.VGRF, VGRF_file.VGRF_Fs,'VGRF', 'Force (N)', ...
+VGRF_filter_IIR.F, 'IIR Bandpass Filter', ...
+VGRF_filter_FIR.testFilter, 'FIR Bandpass Filter', 2);
 
 % BFVdu Signal
 
@@ -34,5 +29,6 @@ BFVdu_file = load('BFVdata_assignment4.mat');
 BFVdu_filter_FIR = load('testFilter.mat');
 BFVdu_filter_IIR = load('testFilter.mat');
 
-plot_all(BFVdu_file.BFVdu, BFVdu_file.BFV_Fs,'BFVdu', 'Blood Flow Velocity (m/s)', BFVdu_filter_FIR.testFilter, 'FIR Bandpass Filter');
-plot_all(BFVdu_file.BFVdu, BFVdu_file.BFV_Fs,'BFVdu', 'Blood Flow Velocity (m/s)', BFVdu_filter_IIR.testFilter, 'IIR Bandpass Filter');
+plot_all(BFVdu_file.BFVdu, BFVdu_file.BFV_Fs,'BFVdu', 'Blood Flow Velocity (m/s)', ...
+BFVdu_filter_IIR.testFilter, 'IIR Bandpass Filter', ...
+BFVdu_filter_FIR.testFilter, 'FIR Bandpass Filter', 3);
--- assignment4/plot_all.m
+++ assignment4/plot_all.m
@@ -1,62 +1,200 @@
-function plot_all(signal, Fs, signal_name, signal_units, filter_obj, filter_name)
-    % Function will plot all 6 of the required plots for each signal and filter.
-    % Plots the original signal on the left and filtered signal on the right.
-    % First row is time domain, second is magnitude spectrum, third is phase spectrum.
+function plot_all(signal, Fs, signal_name, signal_units, ...
+                  IIR_filter_obj, IIR_filter_name, ...
+                  FIR_filter_obj, FIR_filter_name, part)
+    % Function will plot all required plots for each signal and filter.
+    % Unless otherwise mentioned, first row is time domain, second is 
+    % magnitude spectrum, third is phase spectrum.
 
+%-------------------------------------------------------------------------%
+    
+    % plotting non-filtered signal
     figure;
-    sgtitle([signal_name, ' Signal Before and After Filtering with the ' filter_name], 'FontWeight', 'bold');
+    sgtitle([signal_name, ' Signal Before Filtering'], 'FontWeight', 'bold');
 
     % determine time axis
     t = (0:length(signal)-1) / Fs;
 
     % plot time domain signal prefiltering
-    subplot(3,2,1);
-    plot(t, signal);
-    title(['Original ', signal_name, ' Signal']);
+    subplot(3,1,1);
+    plot(t, signal, 'r');
+    title(['Time Domain ', signal_name, ' Signal']);
     xlabel('Time (s)');
     ylabel(signal_units);
 
     % calculate Fourier transform prefiltering
-    [Mx, phx, f] = fourier_dt(signal, Fs, 'full');
+    [Mx, phx, f] = fourier_dt(signal, Fs, 'half');
 
     % plot frequency domain magnitude spectrum prefiltering
-    subplot(3,2,3);
-    plot(f, db(Mx));
-    title(['Magnitude Spectrum of Original ', signal_name, ' Signal']);
+    subplot(3,1,2);
+    plot(f, db(Mx), 'r');
+    title(['Magnitude Spectrum of ', signal_name, ' Signal']);
     xlabel('Frequency (Hz)');
     ylabel('Magnitude (dB)');
 
     % plot frequency domain phase spectrum prefiltering
-    subplot(3,2,5);
-    plot(f, phx);
-    title(['Phase Spectrum of Original ', signal_name, ' Signal']);
+    subplot(3,1,3);
+    plot(f, phx, 'r');
+    title(['Phase Spectrum of ', signal_name, ' Signal']);
     xlabel('Frequency (Hz)');
     ylabel('Phase (radians)');
-
+    
+%-------------------------------------------------------------------------%    
+    
+    % plot the comparison between original and IIR filtered signal
+    figure;
+    sgtitle([signal_name, ' Signal Compared Before and After ', ...
+            IIR_filter_name], 'FontWeight', 'bold');
+    
     % filter the signal
-    filtered_signal = filter(filter_obj, signal);
-
+    IIR_filtered_signal = filter(IIR_filter_obj, signal);
+    
     % plot time domain signal postfiltering
-    subplot(3,2,2);
-    plot(t, filtered_signal);
-    title(['Filtered ', signal_name, ' Signal']);
+    subplot(3,1,1);
+    plot(t, signal, 'r');
+    hold on;
+    plot(t, IIR_filtered_signal, 'g');
+    hold off;
+    title(['Time Domain ', signal_name, ' Signal']);
     xlabel('Time (s)');
     ylabel(signal_units);
+    legend('Original', IIR_filter_name);
 
     % calculate Fourier transform postfiltering
-    [f_Mx, f_phx, f_f] = fourier_dt(filtered_signal, Fs, 'full');
+    [IIR_Mx, IIR_phx, IIR_f] = fourier_dt(IIR_filtered_signal, Fs, 'half');
 
     % plot frequency domain magnitude spectrum postfiltering
-    subplot(3,2,4);
-    plot(f_f, db(f_Mx));
-    title(['Magnitude Spectrum of Filtered ', signal_name, ' Signal']);
+    subplot(3,1,2);
+    plot(f, db(Mx), 'r');
+    hold on;
+    plot(IIR_f, db(IIR_Mx), 'g');
+    hold off;
+    title(['Magnitude Spectrum of ', signal_name, ' Signal']);
     xlabel('Frequency (Hz)');
     ylabel('Magnitude (dB)');
 
     % plot frequency domain phase spectrum postfiltering
-    subplot(3,2,6);
-    plot(f_f, f_phx);
-    title(['Phase Spectrum of Filtered ', signal_name, ' Signal']);
+    subplot(3,1,3);
+    plot(f, phx, 'r');
+    hold on;
+    plot(IIR_f, IIR_phx, 'g');
+    hold off;
+    title(['Phase Spectrum of ', signal_name, ' Signal']);
     xlabel('Frequency (Hz)');
     ylabel('Phase (radians)');
+    
+%-------------------------------------------------------------------------%
+
+    % plot the comparison between original and FIR filtered signal
+    figure;
+    sgtitle([signal_name, ' Signal Compared Before and After ', ...
+            FIR_filter_name], 'FontWeight', 'bold');
+    
+    % filter the signal
+    FIR_filtered_signal = filter(FIR_filter_obj, signal);
+    
+    % plot time domain signal postfiltering
+    subplot(3,1,1);
+    plot(t, signal, 'r');
+    hold on;
+    plot(t, FIR_filtered_signal, 'b');
+    hold off;
+    title(['Time Domain ', signal_name, ' Signal']);
+    xlabel('Time (s)');
+    ylabel(signal_units);
+    legend('Original', FIR_filter_name);
+
+    % calculate Fourier transform postfiltering
+    [FIR_Mx, FIR_phx, FIR_f] = fourier_dt(FIR_filtered_signal, Fs, 'half');
+
+    % plot frequency domain magnitude spectrum postfiltering
+    subplot(3,1,2);
+    plot(f, db(Mx), 'r');
+    hold on;
+    plot(FIR_f, db(FIR_Mx), 'b');
+    hold off;
+    title(['Magnitude Spectrum of ', signal_name, ' Signal']);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
+
+    % plot frequency domain phase spectrum postfiltering
+    subplot(3,1,3);
+    plot(f, phx, 'r');
+    hold on;
+    plot(FIR_f, FIR_phx, 'b');
+    hold off;
+    title(['Phase Spectrum of ', signal_name, ' Signal']);
+    xlabel('Frequency (Hz)');
+    ylabel('Phase (radians)');
+    
+%-------------------------------------------------------------------------%
+    
+    % plot the comparison between original and IIR filtered signal
+    figure;
+    sgtitle(['Filtered ', signal_name, ' Signal Comparing ', ...
+            IIR_filter_name, ' to ', FIR_filter_name], 'FontWeight', 'bold');
+
+    % plot time domain signal postfiltering
+    subplot(3,1,1);
+    plot(t, IIR_filtered_signal, 'g');
+    hold on;
+    plot(t, FIR_filtered_signal, 'b');
+    hold off;
+    title(['Time Domain ', signal_name, ' Signal']);
+    xlabel('Time (s)');
+    ylabel(signal_units);
+    legend(IIR_filter_name, FIR_filter_name);
+
+    % plot frequency domain magnitude spectrum postfiltering
+    subplot(3,1,2);
+    plot(IIR_f, db(IIR_Mx), 'g');
+    hold on;
+    plot(FIR_f, db(FIR_Mx), 'b');
+    hold off;
+    title(['Magnitude Spectrum of ', signal_name, ' Signal']);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
+
+    % plot frequency domain phase spectrum postfiltering
+    subplot(3,1,3);
+    plot(IIR_f, IIR_phx, 'g');
+    hold on;
+    plot(FIR_f, FIR_phx, 'b');
+    hold off;
+    title(['Phase Spectrum of ', signal_name, ' Signal']);
+    xlabel('Frequency (Hz)');
+    ylabel('Phase (radians)');
+
+%-------------------------------------------------------------------------%
+    
+    % plot specific EEG comparison zoomed in on magnitude spectrum
+    if (part == 1)
+        figure;
+        sgtitle('Comparing Magnitude Spectrum of Original and Filtered EEG Signals around 50 Hz');
+        
+        % compare IIR filtered and original signal
+        subplot(2,1,1);
+        plot(f, db(Mx), 'r');
+        hold on;
+        plot(IIR_f, db(IIR_Mx), 'g');
+        hold off;
+        xlim([45 55]);
+        title(['Zoomed In Magnitude Spectrum Comparing Original and ', ...
+               IIR_filter_name, ' Signals']);
+        xlabel('Frequency (Hz)');
+        ylabel('Magnitude (dB)');
+        legend('Original', IIR_filter_name);
+        
+        % compare FIR filtered and original signal
+        subplot(2,1,2);
+        plot(f, db(Mx), 'r');
+        hold on;
+        plot(FIR_f, db(FIR_Mx), 'b');
+        hold off;
+        xlim([45 55]);
+        title(['Zoomed In Magnitude Spectrum Comparing Original and ', ...
+               FIR_filter_name, ' Signals']);
+        xlabel('Frequency (Hz)');
+        ylabel('Magnitude (dB)');
+        legend('Original', FIR_filter_name);
+    end
 end
